d20eb8c8e94a2f54ab379473c3439d25c14f1d18
Saydva <kacurikjozef@gmail.com>
2024-08-18 20:55:52 +0200

    stranka funguje celkom fajn

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
-//Simulacia databazy filmov - nemuselo byt ale je
-const movies = [
-  { id: 1, title: "Matrix", genre: "Scifi", rating: 8.7 },
+// Simulácia databázy filmov - to ste vy robiť nemuseli
+let movies = [
+  { id: 1, title: "Matrix", genre: "Sci-Fi", rating: 8.7 },
   { id: 2, title: "Titanic", genre: "Romance", rating: 7.8 },
   { id: 3, title: "Joker", genre: "Drama", rating: 8.5 },
 ];
 
-//Uvitacia root obrazovka
+// Uvítacia root stránka
 app.get("/", (req, res) => {
-  res.send("Vitajte na nasej webovel stranke s filmami.!");
+  res.send("Vitajte na našej webovej stránke s filmami!");
 });
 
-//Endpoint pre zobrazenie informacii o filme na zaklade ID
-
+// Endpoint pre zobrazenie informácií o filme na základe ID
 app.get("/movies/:id", (req, res) => {
   const movieId = req.params.id;
-  const movie = movies.find((movie) => movieId === parseInt(movieId));
+  const movie = movies.find((movie) => movie.id === parseInt(movieId));
   if (!movie) {
-    return res.status(400).send("Film nenajdeni");
+    return res.status(404).send("Film nenájdený.");
   }
   res.send(movie);
 });
 
-//Endpoint pre triedenie filmov podla hodnotenia
-
-app.get("/movies/", (req, res) => {
-  const sortBy = req.params.sortBy;
+// Endpoint pre triedenie filmov podľa hodnotenia
+app.get("/movies", (req, res) => {
+  const sortBy = req.query.sortBy;
   if (sortBy === "rating") {
     const sortedMovies = movies.sort((a, b) => b.rating - a.rating);
     return res.send(sortedMovies);
   }
-  res.status(400).send("Nespravne zadanie");
+  res.status(400).send("Nesprávne triedenie.");
 });
 
+// Genericky endpoint pre nesprávne cesty
 app.get("*", (req, res) => {
-  res.status(404).send("Starnka neexistuje!!");
+  res.status(404).send("Upsss! Táto stránka neexistuje!");
 });
 
-app.listen(port, () => {
-  console.log(`Server je spusteny na porte${port}`);
+app.listen(3000, () => {
+  console.log("Server spustený na porte 3000");
 });
