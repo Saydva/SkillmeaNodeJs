d4a5078fd947fb27ee1818cfc87963aec4ad539f
Saydva <kacurikjozef@gmail.com>
2024-08-19 22:19:05 +0200

    working on first server get and post

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express = require("express");
 const app = express();
+const { v4: uuid } = require("uuid");
 
-// Simulácia databázy filmov - to ste vy robiť nemuseli
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 let movies = [
-  { id: 1, title: "Matrix", genre: "Sci-Fi", rating: 8.7 },
-  { id: 2, title: "Titanic", genre: "Romance", rating: 7.8 },
-  { id: 3, title: "Joker", genre: "Drama", rating: 8.5 },
+  { id: uuid(), title: "Matrix", genre: "Sci-Fi", rating: 8.7 },
+  { id: uuid(), title: "Titanic", genre: "Romance", rating: 7.8 },
+  { id: uuid(), title: "Joker", genre: "Drama", rating: 8.5 },
 ];
 
 // Uvítacia root stránka
 app.get("/", (req, res) => {
-  res.send("Vitajte na našej webovej stránke s filmami!");
+  res.send({ message: "Vitajte na našej webovej stránke s filmami!" });
 });
 
 // Endpoint pre zobrazenie informácií o filme na základe ID
 app.get("/movies/:id", (req, res) => {
   const movieId = req.params.id;
-  const movie = movies.find((movie) => movie.id === parseInt(movieId));
+  const movie = movies.find((movie) => movie.id === movieId);
   if (!movie) {
-    return res.status(404).send("Film nenájdený.");
+    res.set("Content-Type", "text/plain");
+    return res.status(404).send({ message: "Film nenájdený." });
   }
+  res.set("Content-Type", "aplication/json");
+  res.set("X-powered-By", "HelenSoft");
   res.send(movie);
 });
 
@@ -30,12 +36,29 @@ app.get("/movies", (req, res) => {
     const sortedMovies = movies.sort((a, b) => b.rating - a.rating);
     return res.send(sortedMovies);
   }
-  res.status(400).send("Nesprávne triedenie.");
+  res.status(400).send({ message: "Nesprávne triedenie." });
+});
+
+app.post("/movies", (req, res) => {
+  const { title, genre, rating } = req.body;
+  if (!title || !genre || !rating) {
+    return res.status(400).send({ message: "Chybaju udaje o filme" });
+  }
+  const newMovie = {
+    id: uuid(),
+    title,
+    genre,
+    rating,
+  };
+  movies.push(newMovie);
+  res.status(201).send({
+    message: `Film ${title} bol uspesne vytvoreny pod zanrom ${genre} s hodnotenim ${rating}`,
+  });
 });
 
 // Genericky endpoint pre nesprávne cesty
 app.get("*", (req, res) => {
-  res.status(404).send("Upsss! Táto stránka neexistuje!");
+  res.status(404).send({ message: "Upsss! Táto stránka neexistuje!" });
 });
 
 app.listen(3000, () => {
