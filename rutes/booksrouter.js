2ab0c01a062c3112334182c0ac81795194a3c665
Saydva <kacurikjozef@gmail.com>
2024-08-25 10:29:47 +0200

    using node --watch instead of nodemon

--- rutes/booksrouter.js
+++ rutes/booksrouter.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({ message: "toto som necekal do p...Knihy z GET /books" });
+  res.send({ message: "toto som upscakal do p...Knihy z GET /books" });
+});
+
+router.get("/prekvapko", (req, res) => {
+  res.send({
+    message:
+      "nodemon nepotrebujes, node ma --watch na restart servera.!!!!  A je ovela stabilnejsi hahahahahahahah!!!!!",
+  });
 });
 
 module.exports = router;
